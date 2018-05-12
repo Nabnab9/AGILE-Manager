e22b8167962bfe7e750fcd1a0312d16b95231931
Nabnab <alban.rousseau9@gmail.com>
2018-05-12 17:10:28 +0200

    test sur deploy

--- script_deploy/deploy.sh
+++ script_deploy/deploy.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-echo ${server}
-
 echo '------ START DEPLOY ------'
 echo '--------- pkill java ------'
-sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && pkill java"
-echo '--------- pkill node ------'
-sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && pkill node"
-echo '--------- git pull ------'
-sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && git pull"
-echo '--------- create log folder ------'
-sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager/script_deploy/logs/ && mkdir ${TRAVIS_JOB_NUMBER}-${TRAVIS_COMMIT}"
-echo '--------- start server ------'
-sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && ./mvnw > script_deploy/${TRAVIS_COMMIT}/deploy.mvnw.log 2>&1 & "
-echo '--------- sleep 1m ------'
-sleep 1m
-echo '--------- yarn build ------'
-sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && yarn webpack:build > script_deploy/$TRAVIS_COMMIT/deploy.yarnstart.log 2>&1 &"
+sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && mkdir testdepuistravis"
