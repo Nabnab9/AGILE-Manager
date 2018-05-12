dc2e1f7431cf2ac31051e7801fcafc10482e71dd
Nabnab <alban.rousseau9@gmail.com>
2018-05-12 22:20:28 +0200

    je crois que c'est bon

--- script_deploy/deploy.sh
+++ script_deploy/deploy.sh
@@ -16,9 +16,10 @@ sshpass -p $password ssh -t $username@$server "cd projects/AGILE-Manager && git 
 echo '--------- create log folder ------'
 sshpass -p $password ssh -t $username@$server "cd projects/AGILE-Manager/script_deploy/logs/ && mkdir $TRAVIS_BUILD_NUMBER-$TRAVIS_JOB_NUMBER"
 
+echo '--------- sleep 30s then build app ------'
+sshpass -p $password ssh -t $username@$server "cd projects/AGILE-Manager && yarn webpack:build  > script_deploy/logs/$TRAVIS_BUILD_NUMBER-$TRAVIS_JOB_NUMBER/deploy.yarnbuild.log 2>&1 &"
+
 echo '--------- start server ------'
-sshpass -p $password ssh -t $username@$server "cd projects/AGILE-Manager && ./mvnw > script_deploy/logs/$TRAVIS_BUILD_NUMBER-$TRAVIS_JOB_NUMBER/deploy.mvnw.log 2>&1 & "
+sleep 30s && sshpass -p $password ssh -t $username@$server "cd projects/AGILE-Manager && ./mvnw > script_deploy/logs/$TRAVIS_BUILD_NUMBER-$TRAVIS_JOB_NUMBER/deploy.mvnw.log 2>&1 & "
 
-echo '--------- sleep 2m then yarn start ------'
 
-sleep 2m && sshpass -p $password ssh -t $username@$server "cd projects/AGILE-Manager && yarn start > script_deploy/logs/$TRAVIS_BUILD_NUMBER-$TRAVIS_JOB_NUMBER/deploy.yarnstart.log 2>&1 &"
