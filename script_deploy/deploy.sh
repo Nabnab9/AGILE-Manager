d48ccd1e954836ac92899a7cfd455cf491bf351d
Nabnab <alban.rousseau9@gmail.com>
2018-05-12 15:32:52 +0200

    changement script deploy

--- script_deploy/deploy.sh
+++ script_deploy/deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && pkill java && git pull && chmod +x mvnw && ./mvnw > deploy.log 2>&1 & "
+sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && pkill java"
+sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && pkill node"
+sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && git pull"
+sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && ./mvnw > script_deploy/deploy.mvnw.log 2>&1 & "
+sleep 1m
+sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && yarn webpack:build > script_deploy/deploy.yarnstart.log 2>&1 &"
