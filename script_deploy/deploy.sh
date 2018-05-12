fab24b68715efcbd28342d98719338a90591243d
Nabnab <alban.rousseau9@gmail.com>
2018-05-12 16:48:32 +0200

    test sur deploy

--- script_deploy/deploy.sh
+++ script_deploy/deploy.sh
@@ -7,6 +7,8 @@ echo '--------- pkill node ------'
 sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && pkill node"
 echo '--------- git pull ------'
 sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && git pull"
+echo '--------- create log folder ------'
+sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager/script_deploy/logs/ && mkdir ${TRAVIS_JOB_NUMBER}-${TRAVIS_COMMIT}"
 echo '--------- start server ------'
 sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && ./mvnw > script_deploy/${TRAVIS_COMMIT}/deploy.mvnw.log 2>&1 & "
 echo '--------- sleep 1m ------'
