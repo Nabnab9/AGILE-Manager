989a250e5b27b08b2d1d5226e246a472a65fb3d0
Nabnab9 <alban.rousseau9@gmail.com>
2018-05-12 16:08:41 +0200

    Update deploy.sh

--- script_deploy/deploy.sh
+++ script_deploy/deploy.sh
@@ -3,6 +3,6 @@
 sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && pkill java"
 sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && pkill node"
 sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && git pull"
-sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && ./mvnw > script_deploy/${TRAVIS_COMMIT}/deploy.mvnw.log 2>&1 & "
+sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && ./mvnw > script_deploy/$TRAVIS_COMMIT/deploy.mvnw.log 2>&1 & "
 sleep 1m
-sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && yarn webpack:build > script_deploy/${TRAVIS_COMMIT}/deploy.yarnstart.log 2>&1 &"
+sshpass -p ${password} ssh -t ${username}@${server} "cd projects/AGILE-Manager && yarn webpack:build > script_deploy/$TRAVIS_COMMIT/deploy.yarnstart.log 2>&1 &"
