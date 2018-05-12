4506e5d1805322db82f689f871e9e45fd2853c4b
Nabnab <alban.rousseau9@gmail.com>
2018-05-12 22:30:11 +0200

    yarn install dans le deploy.sh

--- script_deploy/deploy.sh
+++ script_deploy/deploy.sh
@@ -16,6 +16,9 @@ sshpass -p $password ssh -t $username@$server "cd projects/AGILE-Manager && git 
 echo '--------- create log folder ------'
 sshpass -p $password ssh -t $username@$server "cd projects/AGILE-Manager/script_deploy/logs/ && mkdir $TRAVIS_BUILD_NUMBER-$TRAVIS_JOB_NUMBER"
 
+echo '--------- yarn install ------'
+sshpass -p $password ssh -t $username@$server "cd projects/AGILE-Manager && yarn install > script_deploy/logs/$TRAVIS_BUILD_NUMBER-$TRAVIS_JOB_NUMBER/deploy.yarninstall.log 2>&1 &"
+
 echo '--------- sleep 30s then build app ------'
 sshpass -p $password ssh -t $username@$server "cd projects/AGILE-Manager && yarn webpack:build  > script_deploy/logs/$TRAVIS_BUILD_NUMBER-$TRAVIS_JOB_NUMBER/deploy.yarnbuild.log 2>&1 &"
 
