633bb88978240dbe69e00d21c6a1a99319787746
Nabnab9 <alban.rousseau9@gmail.com>
2018-05-13 14:40:10 +0200

    Update deploy.sh

--- script_deploy/deploy.sh
+++ script_deploy/deploy.sh
@@ -18,13 +18,13 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
     sshpass -p $password ssh -t $username@$server "cd projects/AGILE-Manager/script_deploy/logs/ && mkdir $TRAVIS_BUILD_NUMBER-$TRAVIS_JOB_NUMBER"
 
     echo '--------- yarn install ------'
-    sshpass -p $password ssh -t $username@$server "cd projects/AGILE-Manager && yarn install > script_deploy/logs/$TRAVIS_BUILD_NUMBER-$TRAVIS_JOB_NUMBER/deploy.yarninstall.log 2>&1 &"
+    sshpass -p $password ssh -t $username@$server "cd projects/AGILE-Manager && yarn install > script_deploy/logs/$TRAVIS_BUILD_NUMBER-$TRAVIS_JOB_NUMBER/deploy.yarninstall.log 2>&1"
 
     echo '--------- sleep 30s then build app ------'
-    sshpass -p $password ssh -t $username@$server "cd projects/AGILE-Manager && yarn webpack:build  > script_deploy/logs/$TRAVIS_BUILD_NUMBER-$TRAVIS_JOB_NUMBER/deploy.yarnbuild.log 2>&1 &"
+    sshpass -p $password ssh -t $username@$server "cd projects/AGILE-Manager && yarn webpack:build  > script_deploy/logs/$TRAVIS_BUILD_NUMBER-$TRAVIS_JOB_NUMBER/deploy.yarnbuild.log 2>&1"
 
     echo '--------- start server ------'
-    sleep 30s && sshpass -p $password ssh -t $username@$server "cd projects/AGILE-Manager && ./mvnw > script_deploy/logs/$TRAVIS_BUILD_NUMBER-$TRAVIS_JOB_NUMBER/deploy.mvnw.log 2>&1 & "
+    sshpass -p $password ssh -t $username@$server "cd projects/AGILE-Manager && ./mvnw > script_deploy/logs/$TRAVIS_BUILD_NUMBER-$TRAVIS_JOB_NUMBER/deploy.mvnw.log 2>&1 & "
 
     echo '------ END DEPLOY ------'
 else
