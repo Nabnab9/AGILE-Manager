955ca6fa703a4504a2f76328180d89fa627081a8
Ckram <cram.leroux@gmail.com>
2018-05-13 15:39:33 +0200

    Ajout angular material/cdk

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -17,6 +17,7 @@ import { AgileManagerAdminModule } from './admin/admin.module';
 import { AgileManagerAccountModule } from './account/account.module';
 import { AgileManagerEntityModule } from './entities/entity.module';
 import { PaginationConfig } from './blocks/config/uib-pagination.config';
+import { MatCardModule } from "@angular/material";
 // jhipster-needle-angular-add-module-import JHipster will add new module here
 import {
     JhiMainComponent,
@@ -38,6 +39,7 @@ import {
         AgileManagerAdminModule,
         AgileManagerAccountModule,
         AgileManagerEntityModule,
+        MatCardModule
         // jhipster-needle-angular-add-module JHipster will add new module here
     ],
     declarations: [
